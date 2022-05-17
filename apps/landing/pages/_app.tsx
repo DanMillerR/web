6f39589521a77c7c36add917d902bfc87ffcab92
DanMillerR <DanielMillerRose@protonmail.com>
2022-05-17 17:14:44 +0500

    landing appWithTranslation

--- apps/landing/pages/_app.tsx
+++ apps/landing/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { Layout } from "ui"
 import { AppProps } from "next/app"
+import { appWithTranslation } from "next-i18next"
 
 const App = ({ Component, pageProps }: AppProps) => (
   <Layout>
@@ -9,4 +10,4 @@ const App = ({ Component, pageProps }: AppProps) => (
   </Layout>
 )
 
-export default App
+export default appWithTranslation(App)
