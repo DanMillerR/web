a92b9a359448972ec820a55af7c0e693c01338a7
DanMillerR <DanielMillerRose@protonmail.com>
2022-05-20 18:31:40 +0500

    ui: Layout minHeight

--- packages/ui/Layout/index.tsx
+++ packages/ui/Layout/index.tsx
@@ -1,11 +1,12 @@
-import { Box, Container } from '@mui/material'
+import { Box, Container, CssBaseline } from '@mui/material'
 import { FC } from 'react'
 import { Header } from './Header'
 import { Footer } from './Footer'
 import { containerMaxWidth } from 'cnfg'
 
 export const Layout: FC = ({ children }) => (
-    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+    <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+        <CssBaseline />
         <Header />
         <Box sx={{ flexGrow: 1 }}>
             <Container maxWidth={containerMaxWidth}>{children}</Container>
