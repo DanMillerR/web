1666fb74b323671afde3f7263965d602bb1d317b
DanMillerR <DanielMillerRose@protonmail.com>
2022-05-20 18:30:29 +0500

    Header styles

--- packages/ui/Layout/Header/index.tsx
+++ packages/ui/Layout/Header/index.tsx
@@ -1,12 +1,12 @@
 import { Box, Container, Typography } from '@mui/material'
-import { containerMaxWidth } from 'cnfg'
+import { containerMaxWidth, styles } from 'cnfg'
 import { useTranslation } from 'next-i18next'
 
 export const Header = () => {
     const { t } = useTranslation('ui', { keyPrefix: 'header' })
 
     return (
-        <Box>
+        <Box sx={styles.headerSx}>
             <Container maxWidth={containerMaxWidth}>
                 <Typography>{t('title')}</Typography>
             </Container>
