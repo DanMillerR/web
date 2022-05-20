f510aa9f5660050306b128914454007a7e3fe270
DanMillerR <DanielMillerRose@protonmail.com>
2022-05-20 11:30:42 +0500

    cnfg ui, colors

--- packages/cnfg/index.ts
+++ packages/cnfg/index.ts
@@ -1,1 +1,24 @@
 export const containerMaxWidth = "xs"
+
+const color = (level: number) => {
+  const color = level.toString(16)
+
+  return "#" + color.repeat(3)
+}
+
+const range = (max: number, min = 0) => {
+  const arr = []
+
+  for (let i = min; i < max; arr.push(i++)) {}
+
+  return arr
+}
+
+export const ui = {
+  colors: {
+    bg: range(16).map((level) => color(level)),
+    text: range(16)
+      .reverse()
+      .map((level) => color(level)),
+  },
+}
