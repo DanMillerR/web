0f33936cee6511ebda0074b39dde2c540da4d088
DanMillerR <DanielMillerRose@protonmail.com>
2022-05-20 18:30:18 +0500

    cnfg: styles

--- packages/cnfg/index.ts
+++ packages/cnfg/index.ts
@@ -22,3 +22,14 @@ export const ui = {
             .map((level) => color(level)),
     },
 }
+
+export const styles = {
+    headerSx: {
+        background: ui.colors.bg[3],
+        color: ui.colors.text[3],
+    },
+    footerSx: {
+        background: ui.colors.bg[3],
+        color: ui.colors.text[3],
+    },
+}
