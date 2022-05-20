b5ed1d1a9bd6537ec96f39308804d837187f756c
DanMillerR <DanielMillerRose@protonmail.com>
2022-05-20 18:00:14 +0500

    landing: cnfg is transpiled

--- apps/landing/next.config.js
+++ apps/landing/next.config.js
@@ -1,4 +1,4 @@
-const withTM = require('next-transpile-modules')(['ui'])
+const withTM = require('next-transpile-modules')(['ui', 'cnfg'])
 const { i18n } = require('./next-i18next.config')
 
 module.exports = withTM({
