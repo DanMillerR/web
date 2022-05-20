e3704b3cba92b6b3c95ce4f5ea8048503a64ddbe
DanMillerR <DanielMillerRose@protonmail.com>
2022-05-20 18:31:11 +0500

    ui: Footer styles

--- packages/ui/Layout/Footer.tsx
+++ packages/ui/Layout/Footer.tsx
@@ -1,12 +1,12 @@
 import { Box, Container } from '@mui/material'
-import { containerMaxWidth } from 'cnfg'
+import { containerMaxWidth, styles } from 'cnfg'
 import { useTranslation } from 'next-i18next'
 
 export const Footer = () => {
     const { t } = useTranslation('ui', { keyPrefix: 'footer' })
 
     return (
-        <Box>
+        <Box sx={styles.footerSx}>
             <Container maxWidth={containerMaxWidth}>{t('text')}</Container>
         </Box>
     )
