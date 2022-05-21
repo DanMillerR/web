d1302c459603bb2ed98bc47e41e1ae5e814a8475
DanMillerR <DanielMillerRose@protonmail.com>
2022-05-21 08:52:19 +0500

    cnfg: using utils

--- packages/cnfg/index.ts
+++ packages/cnfg/index.ts
@@ -1,35 +1,3 @@
 export const containerMaxWidth = 'xs'
-
-const color = (level: number) => {
-    const color = level.toString(16)
-
-    return '#' + color.repeat(3)
-}
-
-const range = (max: number, min = 0) => {
-    const arr = []
-
-    for (let i = min; i < max; arr.push(i++)) {}
-
-    return arr
-}
-
-export const ui = {
-    colors: {
-        bg: range(16).map((level) => color(level)),
-        text: range(16)
-            .reverse()
-            .map((level) => color(level)),
-    },
-}
-
-export const styles = {
-    headerSx: {
-        background: ui.colors.bg[3],
-        color: ui.colors.text[3],
-    },
-    footerSx: {
-        background: ui.colors.bg[3],
-        color: ui.colors.text[3],
-    },
-}
+export * from './ui'
+export * from './styles'
---
+++ packages/cnfg/styles.ts
@@ -0,0 +1,12 @@
+import { ui } from './ui'
+
+export const styles = {
+    headerSx: {
+        background: ui.colors.bg[3],
+        color: ui.colors.text[3],
+    },
+    footerSx: {
+        background: ui.colors.bg[3],
+        color: ui.colors.text[3],
+    },
+}
