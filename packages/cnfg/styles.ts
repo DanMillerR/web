769094556079f8926fd596d510cc007e13871e14
DanMillerR <DanielMillerRose@protonmail.com>
2022-05-21 09:07:48 +0500

    cnfg: header's and footer's texts are white now

--- packages/cnfg/styles.ts
+++ packages/cnfg/styles.ts
@@ -3,10 +3,10 @@ import { ui } from './ui'
 export const styles = {
     headerSx: {
         background: ui.colors.bg[3],
-        color: ui.colors.text[3],
+        color: ui.colors.text[0],
     },
     footerSx: {
         background: ui.colors.bg[3],
-        color: ui.colors.text[3],
+        color: ui.colors.text[0],
     },
 }
