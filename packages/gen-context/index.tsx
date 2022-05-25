30de7ecaf67e846f739dce8bc4b8e6179c6dece5
DanMillerR <DanielMillerRose@protonmail.com>
2022-05-26 00:06:52 +0500

    gen-context: types

--- packages/gen-context/index.tsx
+++ packages/gen-context/index.tsx
@@ -1,5 +1,21 @@
-import { createContext } from 'react'
+import { Provider, ComponentType, Context, PropsWithChildren } from 'react'
 
-const genContext = (CustomProvider, customUseContext, defaultValue) => {
+type PropsWithRealProviders<T, P> = Record<'RealProvider', Provider<T>> & P
+
+type CustomProvider<T, P> = ComponentType<
+    PropsWithChildren<PropsWithRealProviders<T, P>>
+>
+
+type GetCustomUseContext<T> = (
+    context: Context<T>
+) => <ST extends T = T>() => ST
+
+type WrapperProvider<P> = (p: PropsWithChildren<P>) => JSX.Element
+
+export const genContext = <T, P>(
+    CustomProvider: CustomProvider<T, P>,
+    getCustomUseContext: GetCustomUseContext<T>,
+    defaultValue: T
+): [ReturnType<GetCustomUseContext<T>>, WrapperProvider<P>] => {
     return []
 }
