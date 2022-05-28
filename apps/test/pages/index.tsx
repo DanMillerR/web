b0059a96650dbef5621ce025f71bb7710d934add
DanMillerR <DanielMillerRose@protonmail.com>
2022-05-28 18:01:16 +0500

    test app

--- apps/test/pages/index.tsx
+++ apps/test/pages/index.tsx
@@ -4,35 +4,39 @@ import { useContext } from 'react'
 const [useAlert, AlertProvider] = genContext<
     number,
     {
-        arg1: number
+        phrase: 'HI' | 'BYE'
+    },
+    {
+        power: number
     }
 >(
-    ({ RealProvider, arg1, children }) => <div>{children}</div>,
-    (context) => {
-        return <ST extends number = number>() => {
+    ({ RealProvider, phrase, children }) => (
+        <div style={{ background: phrase == 'BYE' ? '#f99' : '#99f' }}>
+            <RealProvider value={10 + 2}>{children}</RealProvider>
+        </div>
+    ),
+    (context) =>
+        ({ power }) => {
             const val = useContext(context)
 
-            return (val + 1) as ST
-        }
-    },
+            return val ** power
+        },
     10
 )
 
 const A = () => {
-    const value = useAlert()
+    const value = useAlert({ power: 3 })
 
-    return <h1>A{value}</h1>
+    return <h1>{value}</h1>
 }
 
-const Index = () => () =>
-    (
-        <>
-            dfasdf
+const Index = () => (
+    <>
+        <A />
+        <AlertProvider phrase="BYE">
             <A />
-            <AlertProvider arg1={20}>
-                <A />
-            </AlertProvider>
-        </>
-    )
+        </AlertProvider>
+    </>
+)
 
 export default Index
