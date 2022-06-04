59f054206596c4ec0738981efb906f72889e3bb9
DanMillerR <DanielMillerRose@protonmail.com>
2022-06-04 20:33:58 +0500

    utils: range linted

--- packages/utils/range.ts
+++ packages/utils/range.ts
@@ -1,7 +1,9 @@
 export const range = (max: number, min = 0) => {
     const arr = []
 
-    for (let i = min; i < max; arr.push(i++)) {}
+    for (let i = min; i < max; i++) {
+        arr.push(i)
+    }
 
     return arr
 }
