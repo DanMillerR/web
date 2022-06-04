d316189d3b6dfbf5f5c8f86424368db2bbaaf059
DanMillerR <DanielMillerRose@protonmail.com>
2022-06-04 20:32:58 +0500

    one, global lint and ts config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,18 @@
 module.exports = {
     root: true,
-    extends: ['custom'],
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['tsconfig.json'],
+    },
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'next',
+        'prettier',
+    ],
+    plugins: ['@typescript-eslint'],
     settings: {
         next: {
             rootDir: ['apps/*/'],
