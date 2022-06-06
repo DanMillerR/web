46c47251a81d81784d0cb320a2635b060623acd3
DanMillerR <DanielMillerRose@protonmail.com>
2022-06-06 13:28:20 +0500

    ui: footer text align

--- packages/ui/Layout/Footer.tsx
+++ packages/ui/Layout/Footer.tsx
@@ -1,9 +1,14 @@
 import { styles } from 'cnfg'
 import { useTranslation } from 'next-i18next'
 import { BlockCover } from '..'
+import { Typography } from '@mui/material'
 
 export const Footer = () => {
     const { t } = useTranslation('ui', { keyPrefix: 'footer' })
 
-    return <BlockCover sx={styles.footerSx}>{t('text')}</BlockCover>
+    return (
+        <BlockCover sx={styles.footerSx}>
+            <Typography textAlign="center">{t('text')}</Typography>
+        </BlockCover>
+    )
 }
