1ed0838c33d0d03d9044e0fd319e1e76f450b6df
DanMillerR <DanielMillerRose@protonmail.com>
2022-06-18 01:22:07 +0500

    ui: Link

---
+++ packages/ui/components/Link.tsx
@@ -0,0 +1,15 @@
+import { Link as MuiLink, LinkProps as MuiLinkProps } from '@mui/material'
+import NextLink, { LinkProps as NextLinkProps } from 'next/link'
+
+export const Link = ({
+    href,
+    next,
+    ...props
+}: MuiLinkProps & {
+    href: NextLinkProps['href']
+    next: Omit<NextLinkProps, 'href'>
+}) => (
+    <NextLink passHref {...{ ...next, href }}>
+        <MuiLink {...props}>{props.children}</MuiLink>
+    </NextLink>
+)
--- packages/ui/components/index.ts
+++ packages/ui/components/index.ts
@@ -1,2 +1,3 @@
 export * from './Layout'
 export * from './BlockCover'
+export * from './Link'
