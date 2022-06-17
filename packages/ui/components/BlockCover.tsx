53dcef3273e6d70ac11f8545ebe75f0673e93fc1
DanMillerR <DanielMillerRose@protonmail.com>
2022-06-18 01:45:42 +0500

    ui: BlockCover: containerProps

--- packages/ui/components/BlockCover.tsx
+++ packages/ui/components/BlockCover.tsx
@@ -1,8 +1,14 @@
-import { Box, BoxProps, Container } from '@mui/material'
+import { Box, BoxProps, Container, ContainerProps } from '@mui/material'
 import { containerMaxWidth } from 'cnfg'
 
-export const BlockCover = ({ children, ...props }: BoxProps) => (
+export const BlockCover = ({
+    children,
+    containerProps,
+    ...props
+}: BoxProps & { containerProps: ContainerProps }) => (
     <Box {...props}>
-        <Container maxWidth={containerMaxWidth}>{children}</Container>
+        <Container {...containerProps} maxWidth={containerMaxWidth}>
+            {children}
+        </Container>
     </Box>
 )
