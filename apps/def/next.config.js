1f97685e2ada4f26bb3099150ff76ad899904d7b
DanMillerR <DanielMillerRose@protonmail.com>
2022-07-05 10:34:18 +0500

    def: next.config.js: transpiled modules

--- apps/def/next.config.js
+++ apps/def/next.config.js
@@ -1,4 +1,9 @@
-const withTM = require('next-transpile-modules')(['ui', 'cnfg', 'gen-context'])
+const withTM = require('next-transpile-modules')([
+    'ui',
+    'cnfg',
+    'utils',
+    'gen-context',
+])
 const { i18n } = require('./next-i18next.config')
 
 module.exports = withTM({ i18n, reactStrictMode: true })
