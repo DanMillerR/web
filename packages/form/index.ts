a72d1f8733cc02f99b64e9296a0357814884685c
DanMillerR <DanielMillerRose@protonmail.com>
2022-07-06 14:42:42 +0500

    form: types

--- packages/form/index.ts
+++ packages/form/index.ts
@@ -4,22 +4,22 @@ type Obj<T extends string> = {
     [K in T]: string
 }
 
-type SetProp = (prop: string, text: string) => void
-type GetOnInput = (
-    prop: string
+type SetProp<T> = (prop: T, text: string) => void
+type GetOnInput<T> = (
+    prop: T
 ) => ({ target }: ChangeEvent<HTMLInputElement>) => void
 
 export const useData = <T extends string>(
     ...props: T[]
-): [Obj<T>, GetOnInput] => {
+): [Obj<T>, GetOnInput<T>] => {
     const [value, setValue] = useState<Obj<T>>(
         props.reduce((o, key) => ({ ...o, [key]: '' }), {}) as Obj<T>
     )
 
-    const setProp: SetProp = (prop, text) =>
+    const setProp: SetProp<T> = (prop, text) =>
         setValue((p) => ({ ...p, [prop]: text }))
 
-    const getOnInput: GetOnInput =
+    const getOnInput: GetOnInput<T> =
         (prop) =>
         ({ target }) =>
             setProp(prop, target.value)
