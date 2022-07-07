9473da5f15c87cbe972ba2c06114e532baa21e60
DanMillerR <DanielMillerRose@protonmail.com>
2022-07-07 08:17:51 +0500

    acc: sign up page

--- apps/acc/pages/up.tsx
+++ apps/acc/pages/up.tsx
@@ -2,17 +2,33 @@ import { useData } from 'form'
 import { Button, TextField } from '@mui/material'
 import { useTranslation } from 'next-i18next'
 import { SyntheticEvent } from 'react'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { updateProfile, createUserWithEmailAndPassword } from 'firebase/auth'
 import { auth } from 'fb'
+import { doc, setDoc } from 'firebase/firestore'
+import { db } from 'fb'
 
 const Up = () => {
     const { t } = useTranslation('sign-up', { keyPrefix: 'with-email' })
-    const [{ email, password }, control] = useData('email', 'name', 'password')
+    const [{ name, email, password }, control] = useData(
+        'email',
+        'name',
+        'password'
+    )
 
     const handleSubmit = (ev: SyntheticEvent) => {
         ev.preventDefault()
 
-        createUserWithEmailAndPassword(auth, email, password)
+        createUserWithEmailAndPassword(auth, email, password).then(
+            async ({ user }) => {
+                await setDoc(doc(db, 'users/' + user.uid), {
+                    email,
+                    password,
+                    name,
+                })
+
+                await updateProfile(user, { displayName: name })
+            }
+        )
     }
 
     return (
