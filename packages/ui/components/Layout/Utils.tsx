58d1a0833c45088649f9a6484164ead5f6afaffd
DanMillerR <DanielMillerRose@protonmail.com>
2022-07-07 08:35:34 +0500

    ui: Utils: useAccounts

--- packages/ui/components/Layout/Utils.tsx
+++ packages/ui/components/Layout/Utils.tsx
@@ -1,11 +1,16 @@
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
 import { PropsWithChildren } from 'react'
+import { useAccounts } from 'acc'
 
 export const Utils = ({
     children,
-}: PropsWithChildren<Record<never, never>>) => (
-    <ThemeProvider theme={createTheme({})}>
-        <CssBaseline enableColorScheme />
-        {children}
-    </ThemeProvider>
-)
+}: PropsWithChildren<Record<never, never>>) => {
+    useAccounts()
+
+    return (
+        <ThemeProvider theme={createTheme({})}>
+            <CssBaseline enableColorScheme />
+            {children}
+        </ThemeProvider>
+    )
+}
