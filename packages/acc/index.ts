d67d4b39dcb44a83a8bbcf8f7467a3d9af961a54
DanMillerR <DanielMillerRose@protonmail.com>
2022-07-07 08:36:10 +0500

    accp: useAccounts, comments

--- packages/acc/index.ts
+++ packages/acc/index.ts
@@ -1,5 +1,8 @@
 import { User } from 'firebase/auth'
+import { useEffect } from 'react'
+import { useLocalConfig } from 'ctx'
 
+// return user's data
 export const useUser = (): [
     { u: User; uLoad: boolean; uErr: Error },
     {
@@ -14,4 +17,38 @@ export const useUser = (): [
 ] => {
     // @ts-ignore
     return [{}, {}]
+}
+
+// manages, what to do with user
+export const useAccounts = () => {
+    const {
+        signInRequired,
+        emailVerificationRequired,
+        additionalRequiredProps,
+    } = useLocalConfig().user
+    const [{ u, uLoad, uErr }, { ud, udErr, udLoad }] = useUser()
+
+    useEffect(() => {
+        if (uLoad) return // todo: showuserLoadingCaution()
+        if (uErr) return // todo: showUserErrorCaution()
+
+        if (signInRequired && !u) return // todo: redirectToSignIn()
+
+        if (u) {
+            if (emailVerificationRequired && !u.emailVerified) return // todo: showEmailVerificationCaution()
+
+            if (additionalRequiredProps[0]) {
+                if (udErr) return // todo: showUserDataErrorCaution()
+                if (udLoad) return // todo: showUserDataLoadingCaution()
+
+                for (const additionalRequiredProp of additionalRequiredProps)
+                    if (
+                        !additionalRequiredProps.includes(
+                            additionalRequiredProp
+                        )
+                    )
+                        return // todo: showAdditionalInformationCaution()
+            }
+        }
+    }, [u, uLoad, uErr, ud, udErr, udLoad])
 }
