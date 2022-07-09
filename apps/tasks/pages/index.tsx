a81be941eafc9abfcefa7965dba8b92eaf38bf32
DanMillerR <DanielMillerRose@protonmail.com>
2022-07-09 23:56:39 +0500

    tasks: v0.01

--- apps/tasks/pages/index.tsx
+++ apps/tasks/pages/index.tsx
@@ -1,6 +1,33 @@
+import { useUser } from 'acc'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { Task } from '../types'
+import { Card, CardMedia, Typography } from '@mui/material'
+import { Flex } from 'ui'
 
-const Index = () => <></>
+const Index = () => {
+    //@ts-ignore
+    const [, { tasks }] = useUser()
+
+    return (
+        <>
+            {Object.values(tasks as Record<string, Task>).map(
+                ({ title, img, text, id }) => (
+                    <Card sx={{ display: 'flex' }} key={id}>
+                        <CardMedia
+                            component="img"
+                            sx={{ width: 151 }}
+                            image={img}
+                        />
+                        <Flex clmn>
+                            <Typography variant="subtitle2">{title}</Typography>
+                            <Typography variant="body1">{text}</Typography>
+                        </Flex>
+                    </Card>
+                )
+            )}
+        </>
+    )
+}
 
 export default Index
 export const getStaticProps = async () => ({
