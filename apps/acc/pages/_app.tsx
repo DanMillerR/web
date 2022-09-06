490a047aee0a204f25183f8925c263215cd5f301
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-07 04:43:27 +0500

    acc: LocalConfig

--- apps/acc/pages/_app.tsx
+++ apps/acc/pages/_app.tsx
@@ -2,13 +2,25 @@ import { Layout } from 'ui'
 import { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
 import { SnackbarProvider } from 'notistack'
+import { LocalConfig } from 'ctx'
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <Layout>
-    <SnackbarProvider>
-      <Component {...pageProps} />
-    </SnackbarProvider>
-  </Layout>
+  <LocalConfig
+    //@ts-ignore
+    value={{
+      user: {
+        additionalRequiredProps: [],
+        emailVerificationRequired: false,
+        signInRequired: false,
+      },
+    }}
+  >
+    <Layout>
+      <SnackbarProvider>
+        <Component {...pageProps} />
+      </SnackbarProvider>
+    </Layout>
+  </LocalConfig>
 )
 
 export default appWithTranslation(App)
