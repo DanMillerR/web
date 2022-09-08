1e307abb54ea95ea3abb0eb116fdad6c8df77264
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-09 01:43:52 +0500

    ui: BlockCover: using LocalConfig

--- packages/ui/components/BlockCover.tsx
+++ packages/ui/components/BlockCover.tsx
@@ -1,14 +1,18 @@
 import { Box, BoxProps, Container, ContainerProps } from '@mui/material'
-import { CONTAINER_MAX_WIDTH } from 'cnfg'
+import { useLocalConfig } from 'ctx'
 
 export const BlockCover = ({
   children,
   containerProps,
   ...props
-}: BoxProps & { containerProps?: Omit<ContainerProps, 'maxWidth'> }) => (
-  <Box {...props}>
-    <Container {...containerProps} maxWidth={CONTAINER_MAX_WIDTH}>
-      {children}
-    </Container>
-  </Box>
-)
+}: BoxProps & { containerProps?: Omit<ContainerProps, 'maxWidth'> }) => {
+  const { CONTAINER_MAX_WIDTH } = useLocalConfig()
+
+  return (
+    <Box {...props}>
+      <Container {...containerProps} maxWidth={CONTAINER_MAX_WIDTH}>
+        {children}
+      </Container>
+    </Box>
+  )
+}
