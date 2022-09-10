921fdbbf5efeddff6fe7adf3946a7495576842ca
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-10 05:21:39 +0500

    accp: useRequireUser: return-url

--- packages/acc/useRequireUser.ts
+++ packages/acc/useRequireUser.ts
@@ -2,7 +2,9 @@ import { useRouter } from 'next/router'
 import { url } from 'cnfg/acc'
 
 export const useRequireUser = () => {
-  const { replace } = useRouter()
+  const { pathname, replace } = useRouter()
+  const replaceUrl = new URL(url)
+  replaceUrl.searchParams.set('return-url', pathname)
 
-  return () => replace(url)
+  return () => replace(replaceUrl)
 }
