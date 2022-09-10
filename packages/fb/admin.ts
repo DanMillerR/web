13b4ef1b19c3367c8f1682ab4834effc127b5030
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-10 05:20:08 +0500

    fb/admin

---
+++ packages/fb/admin.ts
@@ -0,0 +1,12 @@
+import admin from 'firebase-admin'
+import { getFirestore } from 'firebase-admin/firestore'
+import { getAuth } from 'firebase-admin/auth'
+
+var serviceAccount = require('./serviceAccountKey.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+})
+
+export const db = getFirestore(),
+  auth = getAuth()
