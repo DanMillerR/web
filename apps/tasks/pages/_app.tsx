14d97ee641f230a8c8328a86c4f2162d6fea6946
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-13 01:56:24 +0500

    tasks: LocalConfig

--- apps/tasks/pages/_app.tsx
+++ apps/tasks/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { Layout } from 'ui'
 import { AppProps } from 'next/app'
 import { appWithTranslation } from 'next-i18next'
+import { LocalConfig } from 'ctx'
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
+  <LocalConfig>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </LocalConfig>
 )
 
 export default appWithTranslation(App)
