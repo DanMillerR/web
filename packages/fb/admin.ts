6c9536ee9fd9694243c2361d1cc5e003a55cae29
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-13 17:23:15 +0500

    fb: admin: only one app

--- packages/fb/admin.ts
+++ packages/fb/admin.ts
@@ -1,12 +1,14 @@
 import admin from 'firebase-admin'
 import { getFirestore } from 'firebase-admin/firestore'
 import { getAuth } from 'firebase-admin/auth'
+import { getApp } from 'firebase-admin/app'
 
 var serviceAccount = require('./serviceAccountKey.json')
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-})
+if (!getApp())
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  })
 
 export const db = getFirestore(),
   auth = getAuth()
