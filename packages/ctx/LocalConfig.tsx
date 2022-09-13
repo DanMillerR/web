23f916ced9f7e61fc5f5ae477cb97e878d466ad9
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-13 17:21:43 +0500

    ctx: LocalConfig: deep assigning

--- packages/ctx/LocalConfig.tsx
+++ packages/ctx/LocalConfig.tsx
@@ -1,10 +1,17 @@
 import * as config from 'cnfg'
-import { genContext, getCustomProviderWithValueArg } from 'gen-context'
+import { genContext } from 'gen-context'
 import { PropsWithChildren } from 'react'
-import { PropsWithRealProviders } from 'gen-context/types'
+import assign from 'object-assign-deep'
+import { PartialRecursively } from 'types'
 
 export const [useLocalConfig, LocalConfig] = genContext<
   typeof config,
-  PropsWithChildren<{ value: typeof config }>
-  //@ts-ignore
->(config, getCustomProviderWithValueArg<typeof config>())
+  PropsWithChildren<{
+    value?: PartialRecursively<typeof config>
+  }>
+>({
+  defaultValue: config,
+  provider: ({ value, RealProvider, children }) => (
+    <RealProvider value={assign({}, config, value)}>{children}</RealProvider>
+  ),
+})
