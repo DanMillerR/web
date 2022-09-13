38579d4c00882794e370e12821b2efa33e28553d
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-13 17:08:14 +0500

    acc: api: in: messages, await

--- apps/acc/pages/api/in.ts
+++ apps/acc/pages/api/in.ts
@@ -4,8 +4,16 @@ import { NextApiHandler } from 'next'
 const In: NextApiHandler = async ({ query: { uid, password } }, res) => {
   const realPassword = (await db.doc('users/' + uid).get()).get('password')
 
-  if (uid && password && realPassword && realPassword === password)
-    res.json({ token: auth.createCustomToken(uid as string) })
+  if (!uid) return res.json({ message: 'input-uid' })
+  if (!password) return res.json({ message: 'input-password' })
+  if (!realPassword) return res.json({ message: 'user-not-exist' })
+
+  if (realPassword === password)
+    return res.json({
+      token: await auth.createCustomToken(uid as string),
+    })
+
+  res.json({ message: 'smth-went-wrong' })
 }
 
 export default In
