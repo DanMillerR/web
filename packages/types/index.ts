1e1f2ce6a899469e82cd6d58c81a8aa6373bed5f
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-13 17:21:01 +0500

    types: PartialRecursively, EmptyObj: never, never

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,2 +1,10 @@
-export type EmptyObj = Record<string, never>
+export type EmptyObj = Record<never, never>
 export type AnyObj = Record<string, unknown>
+
+export type PartialRecursively<T> = T extends AnyObj
+  ? Partial<{
+      [K in keyof T]: T[K] extends AnyObj
+        ? Partial<PartialRecursively<T[K]>>
+        : T[K]
+    }>
+  : T
