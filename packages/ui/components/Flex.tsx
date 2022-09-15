640c9a466da408352634ae116b7bae11b5969975
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-15 07:04:08 +0500

    ui: Flex: aic

--- packages/ui/components/Flex.tsx
+++ packages/ui/components/Flex.tsx
@@ -2,14 +2,16 @@ import { Box, BoxProps } from '@mui/material'
 
 export const Flex = ({
   clmn,
+  aic,
   ...props
-}: Partial<Record<'clmn', boolean>> & BoxProps) => (
+}: Partial<Record<'clmn' | 'aic', boolean>> & BoxProps) => (
   <Box
     {...props}
     sx={{
       ...props.sx,
       display: 'flex',
       ...(clmn ? { flexDirection: 'column' } : {}),
+      ...(aic ? { alignItems: 'center' } : {}),
     }}
   />
 )
