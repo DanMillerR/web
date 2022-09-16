468ca2bc6edc0b9c670bc075aa09ef15c231dc8f
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-16 09:06:23 +0500

    ui: Layout: Header: using BasicHeader

--- packages/ui/components/Wrapper/Layout/Header/index.tsx
+++ packages/ui/components/Wrapper/Layout/Header/index.tsx
@@ -1,18 +1,8 @@
-import { Typography } from '@mui/material'
-import { styles } from 'cnfg'
-import { useTranslation } from 'next-i18next'
-import { Flex, BlockCover } from '../../..'
 import { UserMenu } from './UserMenu'
+import { Header as BaseHeader } from '../../Header'
 
-export const Header = () => {
-  const { t } = useTranslation('ui', { keyPrefix: 'header' })
-
-  return (
-    <BlockCover sx={styles.headerSx}>
-      <Flex aic sx={{ p: 1 }}>
-        <Typography sx={{ mr: 'auto' }}>{t('title')}</Typography>
-        <UserMenu />
-      </Flex>
-    </BlockCover>
-  )
-}
+export const Header = () => (
+  <BaseHeader>
+    <UserMenu />
+  </BaseHeader>
+)
