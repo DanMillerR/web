fa9a4d3ec1a41fe8528965368d8a88d5d09d569b
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-16 09:17:26 +0500

    cnfg: taskPreview: buttons: useOnClicks, typed, settings button

--- packages/cnfg/tasks/taskPreview.ts
+++ packages/cnfg/tasks/taskPreview.ts
@@ -1,30 +1,86 @@
 import { Variant } from '@mui/material/styles/createTypography'
+import { SvgIconComponent, Close, Done, Settings } from '@mui/icons-material'
+import { updateDoc, doc } from 'firebase/firestore'
+import { db } from 'fb'
+import { useUser } from 'acc'
+import { useTask } from 'ctx'
 import {
-  Close,
-  Done,
-  // Settings
-} from '@mui/icons-material'
+  TASK_DONE_STATE,
+  TASK_FAILED_STATE,
+  TASK_SKIPPED_STATE,
+  TASK_NORMAL_STATE,
+} from './state'
+import { useRouter } from 'next/router'
+import { useTaskDetailsPathname } from '../paths'
+import { IconButtonProps } from '@mui/material'
+
+type Buttons = {
+  symbol: SvgIconComponent
+  labelKey: string
+  color: IconButtonProps['color']
+  useOnClick: () => () => void
+}[]
 
 export const textVariant: Variant = 'body1'
-export const buttons = [
+export const buttons: Buttons = [
   {
     symbol: Done,
     labelKey: 'done',
     color: 'success',
+    useOnClick: () => {
+      const [{ uid }] = useUser()
+      const { id, state } = useTask()
+
+      return () =>
+        updateDoc(
+          doc(db, 'users/' + uid),
+          'tasks.' + id + '.state',
+          state == TASK_DONE_STATE ? TASK_NORMAL_STATE : TASK_DONE_STATE
+        )
+    },
   },
   {
     symbol: Close,
     labelKey: 'fail',
     color: 'error',
+    useOnClick: () => {
+      const [{ uid }] = useUser()
+      const { id, state } = useTask()
+
+      return () =>
+        updateDoc(
+          doc(db, 'users/' + uid),
+          'tasks.' + id + '.state',
+          state == TASK_FAILED_STATE ? TASK_NORMAL_STATE : TASK_FAILED_STATE
+        )
+    },
   },
   {
     symbol: Close,
     labelKey: 'skip',
     color: 'warning',
+    useOnClick: () => {
+      const [{ uid }] = useUser()
+      const { id, state } = useTask()
+
+      return () =>
+        updateDoc(
+          doc(db, 'users/' + uid),
+          'tasks.' + id + '.state',
+          state == TASK_SKIPPED_STATE ? TASK_NORMAL_STATE : TASK_SKIPPED_STATE
+        )
+    },
   },
-  // {
-  //   symbol: Settings,
-  //   labelKey: 'settings',
-  //   color: 'info',
-  // },
+  {
+    symbol: Settings,
+    labelKey: 'settings',
+    color: 'info',
+    // todo?: make it "a" tag
+    useOnClick: () => {
+      const { replace } = useRouter()
+      const pathname = useTaskDetailsPathname()
+
+      return () => replace(pathname)
+    },
+  },
 ]
