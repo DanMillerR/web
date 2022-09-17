d64fa480b1093a110b4312c8e0b03ccb5a3f1047
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-17 23:13:43 +0500

    accp: useGoToAcc: catch

--- packages/acc/useGoToAcc.ts
+++ packages/acc/useGoToAcc.ts
@@ -1,13 +1,15 @@
 import { useRouter } from 'next/router'
 import { url } from 'cnfg/acc'
+import { useLoadings } from 'loadings'
 
 export const useGoToAcc = () => {
+  const { error } = useLoadings()
   const { replace } = useRouter()
 
   return () => {
     const replaceUrl = new URL(url)
     replaceUrl.searchParams.set('return-url', location.href)
 
-    replace(replaceUrl)
+    replace(replaceUrl).catch(error)
   }
 }
