f66e4ec37ecfb3c3c76d3e69e7712618dd381633
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-17 23:15:06 +0500

    accp: useManageAccToken: catch

--- packages/acc/useManageAccToken.ts
+++ packages/acc/useManageAccToken.ts
@@ -2,11 +2,13 @@ import axios from 'axios'
 import { url } from 'cnfg/acc'
 import { auth } from 'fb'
 import { signInWithCustomToken } from 'firebase/auth'
+import { useLoadings } from 'loadings'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import { useUser } from './useUser'
 // TODO: CATCH
 const useGetToken = () => {
+  const { loading, error } = useLoadings()
   const { replace } = useRouter()
   const [user, userData] = useUser()
 
@@ -14,35 +16,45 @@ const useGetToken = () => {
     const isDataLoaded = user && userData
 
     if (isDataLoaded) {
+      loading()
+
       const url = '/api/in?uid=' + user.uid + '&password=' + userData.password
 
-      axios(url).then(({ data: { token } }) => {
-        if (!token) return
+      axios(url)
+        .then(({ data: { token } }: { data: { token: string } }) => {
+          // todo
+          if (!token) return error(new Error('no-token'))
 
-        const returnUrl = new URL(
-          new URL(location.href).searchParams.get('return-url') as string
-        )
+          const returnUrl = new URL(
+            new URL(location.href).searchParams.get('return-url') as string
+          )
 
-        returnUrl.searchParams.set('acc-token', token)
-        replace(returnUrl)
-      })
+          returnUrl.searchParams.set('acc-token', token)
+          replace(returnUrl).catch(error)
+        })
+        .catch(error)
     }
   }
 }
 
 const useSignInWithToken = () => {
+  const { loading, error } = useLoadings()
   const { replace } = useRouter()
 
   return () => {
     const token = new URL(location.href).searchParams.get('acc-token')
 
     if (token) {
+      loading()
+
       const path = new URL(location.href)
 
       path.searchParams.delete('return-url')
       path.searchParams.delete('acc-token')
 
-      signInWithCustomToken(auth, token).then(() => replace(path))
+      signInWithCustomToken(auth, token)
+        .then(() => replace(path).catch(error))
+        .catch(error)
     }
   }
 }
