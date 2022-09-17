1ae5b9aa718c4497ec7b55f923f0a16cb6a84edc
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-17 23:18:30 +0500

    tasks: sign-out: catch

--- apps/tasks/pages/sign-out.tsx
+++ apps/tasks/pages/sign-out.tsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from 'react'
 import { signOut } from 'firebase/auth'
 import { auth } from 'fb'
+import { useLoadings } from 'loadings'
 
 const SignOut = () => {
+  const { error } = useLoadings()
   const [signedOut, setSignedOut] = useState(false)
 
   useEffect(() => {
     // todo: catch
-    signOut(auth).then(() => setSignedOut(true))
+    signOut(auth)
+      .then(() => setSignedOut(true))
+      .catch(error)
   }, [])
 
   return (
