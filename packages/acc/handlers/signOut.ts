3aa40b5e641e72db936093dad0f518c43782de2f
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-17 23:06:39 +0500

    accp: signOut: catch

--- packages/acc/handlers/signOut.ts
+++ packages/acc/handlers/signOut.ts
@@ -2,15 +2,17 @@ import { signOut as signOutFirebase } from 'firebase/auth'
 import { useLocalConfig } from 'ctx'
 import { auth } from 'fb'
 import { useRouter } from 'next/router'
+import { useLoadings } from 'loadings'
 
 export const useSignOut = () => {
+  const { error } = useLoadings()
   const { replace } = useRouter()
   const { user } = useLocalConfig()
 
   return () => {
     if (user.signInRequired) {
-      replace('/sign-out')
+      replace('/sign-out').catch(error)
       // todo: catch
-    } else signOutFirebase(auth)
+    } else signOutFirebase(auth).catch(error)
   }
 }
