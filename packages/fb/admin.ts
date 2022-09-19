54597e86fee324a3cacf2034380e5e2bc62572c1
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-19 10:36:38 +0500

    fb: admin: import

--- packages/fb/admin.ts
+++ packages/fb/admin.ts
@@ -2,12 +2,11 @@ import admin from 'firebase-admin'
 import { getFirestore } from 'firebase-admin/firestore'
 import { getAuth } from 'firebase-admin/auth'
 import { getApp } from 'firebase-admin/app'
-
-var serviceAccount = require('./serviceAccountKey.json')
+import serviceAccount from './serviceAccountKey.json'
 
 if (!getApp())
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccount as AnyObj),
   })
 
 export const db = getFirestore(),
