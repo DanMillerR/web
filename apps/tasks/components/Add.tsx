7b3f3638c52a8519c5a8efa10583cec691f5860d
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-19 10:35:14 +0500

    tasks: Add: catch

--- apps/tasks/components/Add.tsx
+++ apps/tasks/components/Add.tsx
@@ -4,8 +4,10 @@ import { updateDoc, doc } from 'firebase/firestore'
 import { db } from 'fb'
 import { useUser } from 'acc'
 import { v4 as uuid } from 'uuid'
+import { useLoadings } from 'loadings'
 
 export const Add = () => {
+  const { error } = useLoadings()
   const [{ uid }, { tasks }] = useUser()
   const theme = useTheme()
 
@@ -14,7 +16,7 @@ export const Add = () => {
 
     updateDoc(doc(db, 'users/' + uid), {
       tasks: Object.assign({ [id]: { id } }, tasks),
-    })
+    }).catch(error)
   }
 
   return (
