fce9636879612bd48600f1f01d94c6e015e7f97b
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-20 15:56:23 +0500

    ui: Header: translation config, link to root

--- packages/ui/components/Header.tsx
+++ packages/ui/components/Header.tsx
@@ -3,16 +3,23 @@ import { useTranslation } from 'next-i18next'
 import { PropsWithChildren } from 'react'
 import { BlockCover } from './BlockCover'
 import { Flex } from './Flex'
+import Link from 'next/link'
+import { UI } from 'cnfg/namespaces'
+import { UI_HEADER } from 'cnfg/keyPreffixes'
 
 export const Header = ({ children }: PropsWithChildren<EmptyObj>) => {
-  const { t } = useTranslation('ui', { keyPrefix: 'header' })
+  const { t } = useTranslation(UI, { keyPrefix: UI_HEADER })
 
   return (
     <AppBar position="sticky">
       <BlockCover>
         <Toolbar>
           <Flex aic sx={{ width: '100%' }}>
-            <Typography sx={{ mr: 'auto' }}>{t('title')}</Typography>
+            <Link href="/">
+              <Typography variant="h5" sx={{ mr: 'auto' }}>
+                {t('title')}
+              </Typography>
+            </Link>
             {children}
           </Flex>
         </Toolbar>
