761c71c53e8959d56308f2ea70658d997eeebbe3
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-21 15:57:27 +0500

    fb: admin: using env

--- packages/fb/admin.ts
+++ packages/fb/admin.ts
@@ -2,11 +2,12 @@ import admin from 'firebase-admin'
 import { getFirestore } from 'firebase-admin/firestore'
 import { getAuth } from 'firebase-admin/auth'
 import { getApp } from 'firebase-admin/app'
-import serviceAccount from './serviceAccountKey.json'
 
 if (!getApp())
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as AnyObj),
+    credential: admin.credential.cert(
+      JSON.parse(process.env.ADMIN_APP_CONFIG as string)
+    ),
   })
 
 export const db = getFirestore(),
