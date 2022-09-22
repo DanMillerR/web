e1cd1a43d2920202e4abb2abec36c2e19a2c6fd2
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-22 20:10:34 +0500

    ui: Layout Header: show UserMenu only if user exists

--- packages/ui/components/Wrapper/Layout/Header/index.tsx
+++ packages/ui/components/Wrapper/Layout/Header/index.tsx
@@ -1,8 +1,9 @@
 import { UserMenu } from './UserMenu'
 import { Header as BaseHeader } from '../../../Header'
+import { useUser } from 'acc'
 
-export const Header = () => (
-  <BaseHeader>
-    <UserMenu />
-  </BaseHeader>
-)
+export const Header = () => {
+  const [, data] = useUser()
+
+  return <BaseHeader>{data ? <UserMenu /> : null} </BaseHeader>
+}
