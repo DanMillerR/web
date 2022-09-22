faa3521399be9bbf4911d59b75dbccc483506bb3
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-22 13:22:29 +0500

    fb/admin: app fixed

--- packages/fb/admin.ts
+++ packages/fb/admin.ts
@@ -3,12 +3,15 @@ import { getFirestore } from 'firebase-admin/firestore'
 import { getAuth } from 'firebase-admin/auth'
 import { ServiceAccount, getApp } from 'firebase-admin/app'
 
-if (!getApp())
+try {
+  getApp()
+} catch {
   admin.initializeApp({
     credential: admin.credential.cert(
       JSON.parse(process.env.ADMIN_APP_CONFIG as string) as ServiceAccount
     ),
   })
+}
 
 export const db = getFirestore(),
   auth = getAuth()
