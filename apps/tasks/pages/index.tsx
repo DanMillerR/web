c9b875e5553c1f52458db0b65063a03f491cc13e
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-23 11:20:03 +0500

    tasks: index fixed

--- apps/tasks/pages/index.tsx
+++ apps/tasks/pages/index.tsx
@@ -7,12 +7,12 @@ import { TaskProvider } from 'ctx'
 import { LOADINGS, TASK_PREVIEW, UI, USER_LOAD_STATE } from 'cnfg/namespaces'
 
 const Index = () => {
-  const [, { tasks }] = useUser()
+  const [, data] = useUser()
 
   return (
     <>
       <Add />
-      {Object.values((tasks || {}) as Record<string, Task>)
+      {Object.values((data.tasks || {}) as Record<string, Task>)
         .sort((a, b) => a.state - b.state)
         .map((taskProps) => (
           <TaskProvider value={taskProps} key={taskProps.id}>
