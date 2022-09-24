e9b14c0b188aa87175cb2064155f2c1ae44837e7
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-24 07:53:40 +0500

    ui: NextNProgress

--- packages/ui/components/Wrapper/Utils/index.tsx
+++ packages/ui/components/Wrapper/Utils/index.tsx
@@ -1,11 +1,19 @@
-import { CssBaseline } from '@mui/material'
+import { CssBaseline, useTheme } from '@mui/material'
 import { PropsWithChildren } from 'react'
 import { Props, Providers } from './Providers'
 import { UserLoadState } from './UserLoadState'
+import NextNProgress from 'nextjs-progressbar'
 
 export const Utils = ({ children, localConfig }: PropsWithChildren<Props>) => (
   <Providers {...{ localConfig }}>
     <CssBaseline enableColorScheme />
+    <NextNProgress
+      color={useTheme().palette.secondary.main}
+      startPosition={0.3}
+      stopDelayMs={200}
+      height={3}
+      showOnShallow={true}
+    />
     <UserLoadState>{children}</UserLoadState>
   </Providers>
 )
