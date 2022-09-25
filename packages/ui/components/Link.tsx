bdb83e3064dc3da3844e5ea9686d1489b2f912b6
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-25 11:59:16 +0500

    linted types

--- packages/ui/components/Link.tsx
+++ packages/ui/components/Link.tsx
@@ -1,14 +1,12 @@
 import { Link as MuiLink, LinkProps as MuiLinkProps } from '@mui/material'
 import NextLink, { LinkProps as NextLinkProps } from 'next/link'
 
-export const Link = ({
-  href,
-  next,
-  ...props
-}: MuiLinkProps & {
-  href: NextLinkProps['href']
-  next: Omit<NextLinkProps, 'href'>
-}) => (
+export const Link: (
+  p: MuiLinkProps & {
+    href: NextLinkProps['href']
+    next: Omit<NextLinkProps, 'href'>
+  }
+) => JSX.Element = ({ href, next, ...props }) => (
   <NextLink passHref {...{ ...next, href }}>
     <MuiLink {...props}>{props.children}</MuiLink>
   </NextLink>
