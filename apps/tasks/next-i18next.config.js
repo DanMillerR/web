f102abdab8109874ea40727e568719b15a65b52d
DanMillerR <DanielMillerRose@protonmail.com>
2022-09-25 18:41:51 +0500

    atptt

--- apps/tasks/next-i18next.config.js
+++ apps/tasks/next-i18next.config.js
@@ -1,6 +1,11 @@
+const path = require('path')
+
 module.exports = {
   i18n: {
     locales: ['en', 'ru'],
     defaultLocale: 'en',
   },
+  localePath: path.resolve(
+    process.env.NODE_ENV == 'production' ? './locales' : './public/locales'
+  ),
 }
